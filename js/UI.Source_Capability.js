f2682170725165527be06138bb2e95c2e8b2b7bd
Eric Naeseth <enaeseth@gmail.com>
2007-11-29 22:00:30 +0000

    The edit source toolbar buttons now no longer refocus on the editing document.

--- js/UI.Source_Capability.js
+++ js/UI.Source_Capability.js
@@ -13,11 +13,11 @@ UI.Source_Capability = function Source(loki)
 	
 	this._add_button('source.gif', 'Edit HTML Source', function() {
 		loki.show_source_view();
-	});
+	}).autofocus = false;
 	
 	this._add_source_button('source.gif', 'Edit interactively', function() {
 		loki.show_graphical_view();
-	});
+	}).autofocus = false;
 	
 	[this._add_button, this._add_source_button].each(function (add) {
 		add.call(this, 'raw_source.gif', 'Display raw source', function() {
