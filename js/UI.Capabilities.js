c2cdb527443f0f0e87065399705760f62b33576d
Eric Naeseth <enaeseth@gmail.com>
2007-11-29 22:01:28 +0000

    Began work on moving link editing code into a capability.

--- js/UI.Capabilities.js
+++ js/UI.Capabilities.js
@@ -149,11 +149,12 @@ UI.Capabilities = {
 		this.add('italic', UI.Italic_Capability);
 		this.add('underline', UI.Underline_Capability);
 		this.add('clipboard', UI.Clipboard_Capability);
+		this.add('links', UI.Link_Capability);
 		this.add('source', UI.Source_Capability);
 		// ----- END BUNDLED CAPABILITIES -----
 		
 		// ----- BEGIN BUNDLED SETS -----
-		this.put_set('default', ['bold', 'italic', 'clipboard']);
+		this.put_set('default', ['bold', 'italic', 'clipboard', 'links']);
 		// ----- END BUNDLED SETS -----
 		
 		this._bundled_capabilities_added = true;
