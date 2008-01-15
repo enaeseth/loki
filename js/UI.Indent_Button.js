38d122ee95f999b5e34edceb3043f6eb359b8023
Eric Naeseth <enaeseth@gmail.com>
2008-01-15 20:21:43 +0000

    Added a nag message that informs users that the indent/outdent buttons can only be used on lists. While I was at it, I also cleaned up the code of the buttons by moving some common things into a new list helper.

--- js/UI.Indent_Button.js
+++ js/UI.Indent_Button.js
@@ -11,46 +11,32 @@ UI.Indent_Button = function()
 	Util.OOP.inherits(self, UI.Button);
 
 	this.image = 'indent.gif';
-	this.title = 'Increase list depth';
-	this.click_listener = function() 
+	this.title = 'Indent list item(s)';
+	this.helper = null;
+	
+	this.click_listener = function indent_button_onclick() 
 	{
 		// Only indent if we're inside a UL or OL 
 		// (Do this to avoid misuse of BLOCKQUOTEs.)
-		var sel = Util.Selection.get_selection(self._loki.window);
-		var rng = Util.Range.create_range(sel);
-
-		var ul = Util.Range.get_nearest_ancestor_element_by_tag_name(rng, 'UL');
-		var ol = Util.Range.get_nearest_ancestor_element_by_tag_name(rng, 'OL');
-		var ul_or_ol = ul == null ? ol : ul;
-		var li = Util.Range.get_nearest_ancestor_element_by_tag_name(rng, 'LI');
-
-		if ( ul_or_ol != null )
-		{
-			mb('ul_or_ol', ul_or_ol);
-			var more_distant_ul = Util.Node.get_nearest_ancestor_element_by_tag_name(ul_or_ol, 'UL');
-			var more_distant_ol = Util.Node.get_nearest_ancestor_element_by_tag_name(ul_or_ol, 'OL');
-			var more_distant_ul_or_ol = more_distant_ul == null ? more_distant_ol : more_distant_ul;
-			mb('more_distant_ul_or_ol', more_distant_ul_or_ol);
-
+		
+		if (!this._helper)
+			this.helper = (new UI.List_Helper).init(this._loki);
+		
+		var list = this.helper.get_ancestor_list();
+		var li = this.helper.get_list_item();
+		
+		if (list) {
 			// Don't indent first element in a list, if it is not in a nested list.
 			// This is because in such a situation, Gecko "indents" by surrounding
 			// the UL/OL with a BLOCKQUOTE tag. I.e. <ul><li>as|df</li></ul>
 			// --> <blockquote><ul><li>as|df</li></ul></blockquote>
-			if ( li.previousSibling != null || more_distant_ul_or_ol != null )
-			{
-				self._loki.exec_command('Indent');
-				self._loki.document.normalize();
+			
+			if (li.previousSibling || this.helper.get_more_distant_list(list)) {
+				this._loki.exec_command('Indent');
+				this._loki.document.normalize();
 			}
-		}
-	};
-	this.state_querier = function()
-	{
-		var sel = Util.Selection.get_selection(self._loki.window);
-		var rng = Util.Range.create_range(sel);
-		if ( Util.Range.get_nearest_ancestor_element_by_tag_name(rng, 'UL') == null ||
-			 Util.Range.get_nearest_ancestor_element_by_tag_name(rng, 'OL') == null )
-		{
-			// return disabled
+		} else {
+			this.helper.nag_about_indent_use();
 		}
 	};
 };
--- js/UI.Outdent_Button.js
+++ js/UI.Outdent_Button.js
@@ -11,30 +11,22 @@ UI.Outdent_Button = function()
 	Util.OOP.inherits(self, UI.Button);
 
 	this.image = 'outdent.gif';
-	this.title = 'Decrease list depth';
-	// XXX: eventually take the duplicated code here and that in Indent_Button and combine it
-	// into a helper. Not worth it right now.
-	this.click_listener = function() 
+	this.title = 'Unindent list item(s)';
+	this.helper = null;
+	
+	this.click_listener = function outdent_button_onclick() 
 	{
 		// Only outdent if we're inside a UL or OL 
 		// (Do this to avoid misuse of BLOCKQUOTEs.)
-		var sel = Util.Selection.get_selection(self._loki.window);
-		var rng = Util.Range.create_range(sel);
-		if ( Util.Range.get_nearest_ancestor_element_by_tag_name(rng, 'UL') != null ||
-			 Util.Range.get_nearest_ancestor_element_by_tag_name(rng, 'OL') != null )
-		{
-			self._loki.exec_command('Outdent');
+		
+		if (!this._helper)
+			this.helper = (new UI.List_Helper).init(this._loki);
+			
+		if (this.helper.get_ancestor_list()) {
+			this._loki.exec_command('Outdent');
+		} else {
+			this.helper.nag_about_indent_use();
 		}
 	};
-	this.state_querier = function()
-	{
-		var sel = Util.Selection.get_selection(self._loki.window);
-		var rng = Util.Range.create_range(sel);
-		if ( Util.Range.get_nearest_ancestor_element_by_tag_name(rng, 'UL') == null ||
-			 Util.Range.get_nearest_ancestor_element_by_tag_name(rng, 'OL') == null )
-		{
-			// XXX: *should* this be so? -EN
-			// return disabled
-		}
-	};
+	
 };
