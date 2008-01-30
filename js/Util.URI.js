dd704feaf4a677e801ddd1544375717465ba5d92
Eric Naeseth <enaeseth@gmail.com>
2008-01-30 22:42:16 +0000

    Minor changes: - Removed the unused function Util.URI.make_https_or_http (which had a bug in it anyway). - Util.Array.Methods.append now makes sure that the second argument can actually be iterated over using a for loop.

--- js/Util.URI.js
+++ js/Util.URI.js
@@ -188,23 +188,6 @@ Util.URI.append_to_query = function(uri, params)
 }
 
 /**
- * Converts the given uri to either https or http, depending on value of
- * use_https.
- * 
- * @param	uri			the uri
- * @param 	use_https	(boolean) whether to use https or http
- */
-Util.URI.make_https_or_http = function(uri, use_https)
-{
-	if ( this._use_https )
-		uri = uri.replace(new RegExp('^http:', ''), 'https:');
-	else
-		uri = uri.replace(new RegExp('^https:', ''), 'http:');
-	mb('Util.URI.make_https_or_http: made uri:', uri);
-	return uri
-};
-
-/**
  * Strips leading "https:" or "http:" from a uri, to avoid warnings about
  * mixing https and http. E.g.: https://apps.carleton.edu/asdf ->
  * //apps.carleton.edu/asdf.
