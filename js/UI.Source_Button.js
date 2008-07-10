c14411e0210be5e20bf5bbffeb7aec905fe4ff89
Eric Naeseth <enaeseth@gmail.com>
2008-07-10 20:09:47 +0000

    Making the label on the source view button a bit better.

--- js/UI.Source_Button.js
+++ js/UI.Source_Button.js
@@ -11,7 +11,7 @@ UI.Source_Button = function()
 	Util.OOP.inherits(self, UI.Button);
 
 	this.image = 'source.png';
-	this.title = 'Toggle source';
+	this.title = 'Toggle HTML source view';
 	this.show_on_source_toolbar = true;
 	this.click_listener = function() { self._loki.toggle_iframe_textarea(); };
 };
