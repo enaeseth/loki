f39b26e88104ff4d8f25fe3cac684b601a042cd8
Eric Naeseth <enaeseth@gmail.com>
2008-07-17 18:43:57 +0000

    New helper functions: UI.Masseuse#assign_fake_id and UI.Masseuse#remove_fake_id.

--- js/UI.Masseuse.js
+++ js/UI.Masseuse.js
@@ -49,3 +49,34 @@ UI.Masseuse.prototype.init = function(loki)
 	this._loki = loki;
 	return this;
 };
+
+UI.Masseuse.prototype.assign_fake_id = function assign_fake_element_id(elem) {
+	var base = 'az';
+	
+	function random_int(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	
+	function generate_id(length) {
+		var i, id = '_loki_', c;
+		if (!length)
+			length = 6
+		for (i = 0; i < length; ++i) {
+			c = random_int(base.charCodeAt(0), base.charCodeAt(1));
+			id += String.fromCharCode(c);
+		}
+		return (elem.ownerDocument.getElementById(id))
+			? generate_id(length)
+			: id;
+	}
+	
+	if (!elem.id)
+		elem.id = generate_id();
+	return elem.id;
+};
+
+UI.Masseuse.prototype.remove_fake_id = function remove_fake_element_id(elem) {
+	var pattern = /^_loki_[a-z]+$/;
+	if (elem.id && pattern.test(elem.id))
+		elem.removeAttribute('id');
+};
