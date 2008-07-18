f209f6d327599e37ee6089aa9dbc349edd17f222
Eric Naeseth <enaeseth@gmail.com>
2008-07-18 20:17:05 +0000

    Fixing issue 124; list massaging now occurs on all lists that need it.

--- js/UI.UL_OL_Masseuse.js
+++ js/UI.UL_OL_Masseuse.js
@@ -9,55 +9,50 @@ UI.UL_OL_Masseuse = function()
 {
 	var self = this;
 	var _tagnames = ['UL', 'OL'];
+	var is_li = Util.Node.curry_is_tag('LI');
 	Util.OOP.inherits(self, UI.Masseuse);
 
 	this.massage_node_descendants = function(node)
 	{
-		for ( var j in _tagnames )
-		{
-			var uls = node.getElementsByTagName(_tagnames[j]);
-			for ( var i = 0; i < uls.length; i++ )
-			{
-				self.massage_elem(uls[i]);
+		_tagnames.each(function massage_list_tag_descendants(tag) {
+			var lists = $A(node.getElementsByTagName(tag));
+			var i, length;
+			for (i = 0, length = lists.length; i < length; ++i) {
+				self.massage_elem(lists[i]);
 			}
-		}
+		});
 	};
 	
 	this.unmassage_node_descendants = function(node)
 	{
-		for ( var j in _tagnames )
-		{
-			var uls = node.getElementsByTagName(_tagnames[j]);
-			for ( var i = 0; i < uls.length; i++ )
-			{
-				self.unmassage_elem(uls[i]);
+		_tagnames.each(function unmassage_list_tag_descendants(tag) {
+			var lists = $A(node.getElementsByTagName(tag));
+			var i, length;
+			for (i = 0, length = lists.length; i < length; ++i) {
+				self.unmassage_elem(lists[i]);
 			}
-		}
+		});
 	};
 
 	// <ul><li>out<ul><li>in</li></ul></li><li>out again</li></ul>
 	//   -->
 	// <ul><li>out</li><ul><li>in</li></ul><li>out again</li></ul>
-	this.massage_elem = function(ul)
+	this.massage_elem = function massage_list(list)
 	{
-		
-		if ( ul.parentNode.nodeName == 'LI' )
-		{
-			var old_li = ul.parentNode;
-			if ( old_li.nextSibling == null )
-				old_li.parentNode.appendChild(ul);
-			else
-				old_li.parentNode.insertBefore(ul, old_li.nextSibling);
+		var parent = list.parentNode;
+		var next_item;
+		if (parent.nodeName == 'LI') {
+			next_item = Util.Node.next_matching_sibling(parent, is_li);
+			parent.parentNode.insertBefore(list, next_item);
 		}
 	};
 
 	// <ul><li>out</li><ul><li>in</li></ul><li>out again</li></ul>
 	//   -->
 	// <ul><li>out<ul><li>in</li></ul></li><li>out again</li></ul>
-	this.unmassage_elem = function(list)
+	this.unmassage_elem = function unmassage_list(list)
 	{
 		var prev_item;
-		var is_li = Util.Node.curry_is_tag('LI');
 		
 		if (_tagnames.contains(list.parentNode.nodeName)) {
 			prev_item = Util.Node.previous_matching_sibling(list, is_li);
