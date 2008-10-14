e3a4badca031a2a30982ffc43f31ba3690ab3c9a
Eric Naeseth <enaeseth@gmail.com>
2008-10-14 21:38:48 +0000

    Fixing WebKit compatibility.

--- src/theme.js
+++ src/theme.js
@@ -24,9 +24,14 @@ Loki.Theme = Loki.Class.create({
 	
 	_addStyleSheet: function add_style_sheet(document, url) {
 		base2.DOM.bind(document);
-		if (document.querySelector("link[rel=stylesheet][href=" + url + "]")) {
-			// already exists
-			return false;
+		var selector = "link[rel=stylesheet][href=" + url + "]";
+		try {
+			if (document.querySelector(selector)) {
+				// already exists
+				return false;
+			}
+		} catch (e) {
+			// ignore
 		}
 		
 		var link = document.createElement("LINK");
@@ -34,8 +39,25 @@ Loki.Theme = Loki.Class.create({
 		link.type = "text/css";
 		link.href = url;
 		var head = document.querySelector("head");
+		if (!head) {
+			this._fixDocument(document);
+			head = document.querySelector("head");
+		}
 		head.appendChild(link);
 		
 		return true;
+	},
+	
+	_fixDocument: function fix_document_structure(document) {
+		var root = document.documentElement ||
+			document.getElementsByTagName("HTML")[0];
+		var body = document.getElementsByTagName("BODY");
+		if (!body.length) {
+			body = document.createElement("BODY");
+			root.appendChild(body);
+		}
+		
+		var head = document.createElement("HEAD");
+		root.insertBefore(head, body);
 	}
 });
