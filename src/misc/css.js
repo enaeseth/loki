a9f26f400438b3ccf4076c6f68e1a816dca621a2
Eric Naeseth <enaeseth@gmail.com>
2008-12-05 10:42:01 -0600

    Adding Loki.Misc.CSS, a namespace that can parse CSS selectors for you!

---
+++ src/misc/css.js
@@ -0,0 +1,57 @@
+// Namespace: Loki.Misc.CSS
+// Tools for working with CSS and selectors.
+Loki.Misc.CSS = {
+	// Function: parseSelector
+	// Parses a CSS selector. The parser only includes partial selector support.
+	//
+	// Parameters:
+	//     (String) selector - the selector to parse
+	//
+	// Returns:
+	//     (Object) - the parsed selector
+	parseSelector: function parse_css_selector(selector) {
+		return base2.map(selector.split(/\s*,\s*/),
+			Loki.Misc.CSS._parseSimpleSelector, Loki.Misc.CSS);
+	},
+	
+	_parseSimpleSelector: function _parse_simple_css_selector(selector) {
+		var parser = new Loki.Parser(selector);
+		var data = {
+			element: null,
+			id: null,
+			classes: [],
+			attributes: []
+		};
+		
+		data.element = parser.scan(/^\w+/) || '*';
+		
+		var control, part, match, attr;
+		while (!parser.terminated()) {
+			control = parser.scan(/[#\.\[]/);
+			if (!control)
+				break;
+			
+			if (control == "#") {
+				data.id = parser.scan(/[\w\-]+/);
+			} else if (control == ".") {
+				part = parser.scan(/[\w\-]+/);
+				if (part)
+					data.classes.push(part);
+			} else if (control == "[") {
+				part = parser.scanUntil("]");
+				console.debug(part);
+				match = /^([\w\-]+)\s*(?:([~\|\^\$\*]?=)\s*(.*))?$/.exec(part);
+				if (!match)
+					continue;
+				attr = {name: match[1]};
+				if (match.length > 2) {
+					attr.operator = match[2];
+					attr.value = match[3];
+				}
+				data.attributes.push(attr);
+			}
+		}
+		
+		return data;
+	}
+};
