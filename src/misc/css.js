a8c6cad206d91b5abd5e6b1ee7a5d7c0ae9e3cd6
Eric Naeseth <enaeseth@gmail.com>
2008-12-05 12:00:16 -0600

    Adding Loki.Misc.CSS#elementFromSelector. Thanks to Haml for the probable inspiration.

--- src/misc/css.js
+++ src/misc/css.js
@@ -1,6 +1,50 @@
 // Namespace: Loki.Misc.CSS
 // Tools for working with CSS and selectors.
 Loki.Misc.CSS = {
+	// Function: elementFromSelector
+	// Creates a new element from a CSS selector. The element name in the
+	// selector is used (defaults to "div" if none is specified), the ID and
+	// class names are applied, and any attributes are applied ([attr] =>
+	// attr="attr", and [attr=value] => attr="value"). If multiple simple
+	// selectors are given, the values specified in later selectors are
+	// successively merged in.
+	//
+	// Parameters:
+	//     (Document) doc - the document on which to create the element
+	//     (String) selector - the selector to turn into an element
+	//
+	// Returns:
+	//     (Element) - the created element
+	elementFromSelector: function create_element_from_selector(doc, selector) {
+		var data = {element: 'div', classes: [], attributes: []};
+		base2.forEach(Loki.Misc.CSS.parseSelector(selector), function(sd) {
+			if (sd.element != '*')
+				data.element = sd.element;
+			data.id = sd.id;
+			base2.forEach(sd.classes, function(cl) {
+				data.classes.push(cl);
+			});
+			base2.forEach(sd.attributes, function(at) {
+				data.attributes.push(at);
+			});
+		});
+		
+		var attributes = {};
+		if (data.element)
+		if (data.id)
+			attributes.id = data.id;
+		if (data.classes.length > 0)
+			attributes.className = data.classes.join(' ');
+		
+		base2.forEach(data.attributes, function(attr) {
+			if (attr.operator && attr.operator != '=')
+				return;
+			attributes[attr.name] = attr.value || attr.name;
+		});
+		
+		return base2.DOM.Document.build(doc, data.element, attributes);
+	},
+	
 	// Function: parseSelector
 	// Parses a CSS selector. The parser only includes partial selector support.
 	// If you only need to parse a simple selector, try
