6e0f924d8032f9c87d639c8590027e61e3f5c032
Eric Naeseth <enaeseth@gmail.com>
2009-09-01 16:18:27 -0500

    Reorganizing Loki's codebase.

---
+++ src/loki.js
@@ -0,0 +1,24 @@
+/*
+ * Loki: a JavaScript WYSIWYG HTML editor with a focus on semantic markup.
+ *
+ * Copyright © 2006-2009 Carleton College.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+//= require "core/mb.js"
+//= require "core/tinymce.js"
+//= require "core/Util.js"
+//= require "core/UI.js"
