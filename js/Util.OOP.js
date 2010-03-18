b2d828f6b5dc1699b1c4132ed48f764a28979d61
Eric Naeseth <enaeseth@gmail.com>
2010-03-18 16:40:25 -0500

    Cleaning up the Util.OOP code.
    - Restyling and correcting the code and rewriting the comments.
    - Dropped the unused and useless Util.OOP.swiss.
    - Added the convenience function Util.OOP.mixin.

--- js/Util.OOP.js
+++ js/Util.OOP.js
@@ -105,81 +105,75 @@
  * TODO: revise the above text, and talk about inheritance.
  */
 
-Util.OOP = {};
+Util.OOP = {
+	/**
+	 * Extends an object by copying the properties of another object into it.
+	 *
+	 * All of the properties on `source` will be copied into `target`. Any
+	 * existing properties on `target` that had the same name as a property on
+	 * `source` will have their value overwritten.
+	 *
+	 * Returns the target object.
+	 */
+	extend: function extend_object(target, source) {
+		var name;
+		
+		for (name in source) {
+			target[name] = source[name];
+		}
+		
+		return target;
+	},
+	
+	/**
+	 * Copies all the properties of the given module onto the given target's
+	 * prototype.
+	 *
+	 * Equivalent to using Util.OOP.extend with "target.prototype" instead of
+	 * "target".
+	 *
+	 * Returns the given target (not its prototype).
+	 */
+	mixin: function mix_in_module(target, module) {
+		Util.OOP.extend(target.prototype, module);
+		return target;
+	},
+	
+	/**
+	 * Creates a new instance of the given parent class, and copies all of
+	 * that instance's properties onto the given child index.
+	 *
+	 * Any extra parameters passed to this function are passed through to the
+	 * parent class constructor.
+	 *
+	 * Note that this function is used to implement "old-style", closure-based
+	 * inheritance in Loki. It should not be used in new code, unless its use
+	 * is necessary to implement an existing paradigm (e.g.,
+	 * extending UI.Dialog).
+	 */
+	inherits: function object_inherits_from_class(child, parent) {
+		var parent_prototype;
+		var i;
 
-/**
- * Extends an object by mixing in the properties of another object.
- * @param	{object}	target	The object into which things will be mixed
- * @param	{object}	source	The object providing the properties
- * @type object
- * @return target
- */
-Util.OOP.extend = function(target, source)
-{
-	var names = Util.Object.names(source);
-	for (var i = 0; i < names.length; i++) {
-		target[names[i]] = source[names[i]];
+		if (arguments.length < 2) {
+			throw new TypeError('Must provide a child and a parent class.');
+		} else if (arguments.length == 2) {
+			// no arguments need to be passed to the parent constructor
+			parent_prototype = new parent();
+		} else {
+			// pass all additional arguments to the parent constructor
+			// using eval() is the best way known to get all correct behavior,
+			// so think twice about changing this (admittedly ugly) code
+			var arg_list = [];
+			for (i = 0; i < arguments.length; i++) {
+				arg_list.push('arguments[' + i + ']');
+			}
+			
+			eval('parent_prototype = new parent(' + arg_list.join(', ') + ')');
+		}
+		
+		Util.OOP.extend(child, parent_prototype);
+		child.superclass = parent_prototype;
+		child.__superClass__ = parent_prototype; // CoffeeScript convention
 	}
-	
-	return target;
-}
-
-/**
- * Sets up inheritance from parent to child. To use:
- * - Create parent and add parent's methods and properties.
- * - Create child
- * - At beginning of child's constructor, call inherits(parent, child)
- * - Add child's new methods and properties
- * - To call method foo in the parent: this.superclass.foo.call(this, params)
- * - Be careful where you use self and this: in inherited methods, self
- *   will still refer to the superclass, whereas this will refer, properly, to the
- *   child class. If you must use self, e.g. for event listeners, define self
- *   only inside methods, not directly inside the constructor. (Note: The existing
- *   code doesn't follow this advice perfectly; follow this advice, not that code.)
- *
- * Changed on 2007-09-13 by EN: Now calls the parent class's constructor! Any
- * arguments that need to be passed to the constructor can be provided after
- * the child and parent.
- *
- * Inspired by but independent of <http://www.crockford.com/javascript/inheritance.html>.
- *
- * The main problem with just doing something like
- *     child.prototype = new parent();
- * is that methods inherited from the parent can't set properties accessible
- * by methods defined in the child.
- */
-Util.OOP.inherits = function(child, parent)
-{
-	var parent_prototype = null;
-	var nargs = arguments.length;
-	
-	if (nargs < 2) {
-		throw new TypeError('Must provide a child and a parent class.');
-	} else if (nargs == 2) {
-		parent_prototype = new parent;
-	} else {
-		// XXX: Is there really no better way to do this?!
-		//      Something involving parent.constructor maybe?
-		var arg_list = $R(2, nargs).map(function (i) {
-			return 'arguments[' + String(i) + ']';
-		});
-		eval('parent_prototype = new parent(' + arg_list.join(', ') + ')')
-	}
-	
-	Util.OOP.extend(child, parent_prototype);
-	child.superclass = parent_prototype;
-};
-
-/**
- * Sets up inheritance from parent to child, but only copies over the elements
- * in the parent's prototype provided as arguments after the parent class.
- */
-Util.OOP.swiss = function(child, parent)
-{
-	var parent_prototype = new parent;
-    for (var i = 2; i < arguments.length; i += 1) {
-        var name = arguments[i];
-        child[name] = parent_prototype[name];
-    }
-    return child;
 };
